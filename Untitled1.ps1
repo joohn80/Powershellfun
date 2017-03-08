9cc53ce0e94fabdc3c42115930a7d61b836c0e78
joohn80 <johnifrog@gmail.com>
2017-03-08 12:41:21 +0100

    Update Untitled1.ps1

--- Untitled1.ps1
+++ Untitled1.ps1
@@ -1,9 +1,3 @@
-#$Arrcomp = net view | select -Skip 6 | %{$_ -match '^\\\\([^\t ]*)'|out-null;$matches[1]}
-#Clear-Host
-#$Arrcomp = $Arrcomp.Split("`n")
-
-#The part above this text is used to check for all computers on the net but it doesn't work
-
 $ArrComputers = "."
 #This part chooses what computer you're gonna check the info from. "." is the local computer
 
@@ -15,35 +9,26 @@ $computerOS     = Get-CimInstance -CimSession $cim -ClassName Win32_OperatingSys
 $computerCPU    = Get-CimInstance -CimSession $cim -ClassName Win32_Processor
 $computerHDD    = Get-CimInstance -CimSession $cim -ClassName Win32_LogicalDisk
 Get-CimSession | Remove-CimSession
-<#
-Clear-Host
-foreach ($Computer in $Arrcomputers) 
+
+foreach ($computer in $ArrComputers)
 {
-    #This part fetches the information through wmiobject that is located on the computer and puts it in arrays
+    $BIOS   = $computerBIOS  | Where-Object -Property PScomputername -EQ $computer
+    $System = $computerSystem| Where-Object -Property PScomputername -EQ $computer
+    $OS     = $computerOS    | Where-Object -Property PScomputername -EQ $computer
+    $CPU    = $computerCPU   | Where-Object -Property PScomputername -EQ $computer
+    $HDD    = $computerHDD   | Where-Object -Property PScomputername -EQ $computer
     
-    $computerSystem = get-wmiobject Win32_ComputerSystem -Computer $Computer
-    $computerBIOS   = get-wmiobject Win32_BIOS -Computer $Computer
-    $computerOS     = get-wmiobject Win32_OperatingSystem -Computer $Computer
-    $computerCPU    = get-wmiobject Win32_Processor -Computer $Computer
-    $computerHDD    = Get-WmiObject Win32_LogicalDisk -ComputerName $Computer -Filter drivetype=3
-        
-        #All of the parts below writes out the information it got for you with the use of the arrays used on top
-        
-        ""
-        write-host "System Information for: " $computerSystem.Name -BackgroundColor DarkCyan
-        "_______________________________________________________"
-        ""
-        "Manufacturer: " + $computerSystem.Manufacturer
-        "Model: " + $computerSystem.Model
-        "Serial Number: " + $computerBIOS.SerialNumber
-        "CPU: " + $computerCPU.Name
-        "HDD Capacity: "  + "{0:N2}" -f ($computerHDD.Size/1GB) + "GB"
-        "HDD Space: " + "{0:P2}" -f ($computerHDD.FreeSpace/$computerHDD.Size) + " Free (" + "{0:N2}" -f ($computerHDD.FreeSpace/1GB) + "GB)"
-        "RAM: " + "{0:N2}" -f ($computerSystem.TotalPhysicalMemory/1GB) + "GB"
-        "Operating System: " + $computerOS.caption + ", Service Pack: " + $computerOS.ServicePackMajorVersion
-        "User logged In: " + $computerSystem.UserName
-        "Last Reboot: " + $computerOS.ConvertToDateTime($computerOS.LastBootUpTime)
-        ""
-        "_______________________________________________________"
+    New-Object -TypeName psobject -Property @{
+        'computer'        = $computer
+        'Manufacturer'    = $System.Manufacturer
+        'Model'           = $System.Model
+        'Serial_Number'   = $BIOS.SerialNumber
+        'CPU'             = $CPU.Name
+        'HDD_Capacity'    = $HDD.Size
+        'HDD_Space'       = $HDD.FreeSpace
+        'RAM'             = $System.TotalPhysicalMemory
+        'Operating System'= "$($OS.caption) $($OS.ServicePackMajorVersion)"
+        'User_logged'     = $System.UserName
+        'Last_Reboot'     = $OS.LastBootUpTime
+    }
 }
-#>
